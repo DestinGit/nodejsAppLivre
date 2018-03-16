e1f48646cd2f8ea8eac7b614a1bbd451556de1dc
Destin <d.gandobak@gmail.com>
2018-03-16 16:24:28 +0100

    installation du module moment pour les dates et modification de la methode create de Message avec un rows.map((row) => new Message(row))

--- models/message.js
+++ models/message.js
@@ -1,7 +1,20 @@
 let connection = require('../config/db');
+let moment = require('../config/moment');
 
 class Message {
 
+    constructor(row) {
+        this.row = row;
+    }
+
+    get content() {
+        return this.row.content;
+    }
+
+    get create_at() {
+        return moment(this.row.create_at);
+    }
+    
     static create(content, callback) {
         connection.query('INSERT INTO messages SET content = ?, create_at = ?', 
         [content, new Date()], (err, result) => {
@@ -13,7 +26,8 @@ class Message {
     static all(callback) {
         connection.query('SELECT * FROM messages', (err, rows) => {
             if(err) throw err;
-            callback(rows);
+            // A Eclaircir
+            callback(rows.map((row) => new Message(row)));
         })
     }
 }
